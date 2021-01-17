68c12966c66daf0e10a563d29bb1a8ce4e962217
Matteus Magnusson <senth.wallace@gmail.com>
2021-01-17 16:43:02 +0100

    ✨ Mysql backup

---
+++ backuprunner/backup/mysql_backup.py
@@ -0,0 +1,33 @@
+from .backup import Backup
+from subprocess import DEVNULL, run
+from ..config import config
+import sys
+
+
+class MysqlBackup(Backup):
+    def __init__(self) -> None:
+        super().__init__("MySQL")
+
+    def run(self) -> None:
+        # Only run if a MySQL username and password has been supplied
+        if not config.mysql.username and not config.mysql.password:
+            return
+
+        out = DEVNULL
+
+        if config.debug:
+            out = sys.stdout
+
+        run(
+            [
+                "mysqldump",
+                "-u",
+                str(config.mysql.username),
+                "-p",
+                str(config.mysql.password),
+                "-r",
+                self.filepath,
+                "--all-databeses",
+            ],
+            stdout=out,
+        )
