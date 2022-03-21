ebeb8aa92deb34cb23b68a1cfb254d4b59501f1d
Senth <senth.wallace@gmail.com>
2022-03-21 20:50:59 +0100

    Add support for python 3.10

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Environment :: Console",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     setup_requires=["setuptools_scm"],
     python_requires=">=3.8",
